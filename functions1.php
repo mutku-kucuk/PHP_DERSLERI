21a9f8c5c063a76dca81129ed7e2696fb5c4be19
mutku-kucuk <muhammetutku@outlook.com>
2022-02-26 13:48:07 +0300

    first-commit

---
+++ functions1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+<?php  
+$ad="Utku Kucuk";
+echo strlen($ad);
+echo "<br>";
+echo rand(1,50);
+
+?>
+
+</body>
+</html>
