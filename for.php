2121d7b745b89f830c617f058064e8906a200ed5
mutku-kucuk <muhammetutku@outlook.com>
2022-02-26 11:49:27 +0300

    first-commit

---
+++ for.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+ 
+
+
+<?php for ($i=0; $i < 10; $i++) { ?>
+	<?php  	echo $i;?> <br>
+<?php } ?>
+
+<?php  
+//il plakalarını döngü ile selectbox içine atalım.
+
+?>
+
+<select>
+	<?php  
+	for ($i=1; $i <= 81 ; $i++) { ?>
+		<option><?php echo $i ?></option>
+	<?php  }
+	?>
+	
+</select>
+
+
+
+
+</body>
+</html>
